ac12f1a6f531ee18b31620c0d429d732093a1df1
abhygames <139436321+abhygames@users.noreply.github.com>
2023-07-13 18:07:26 +0700

    Create 300car.js

---
+++ 300car.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'd5608e4549f32a0daf6e6a43089bf56e',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/d5608e4549f32a0daf6e6a43089bf56e/invoke.js"></scr' + 'ipt>');
