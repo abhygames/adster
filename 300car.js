9e6679e633939f10380235a5f19a566d59b28e78
abhygames <abhygmng@gmail.com>
2024-07-29 22:35:55 +0700

    Update 300car.js

--- 300car.js
+++ 300car.js
@@ -5,4 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/d5608e4549f32a0daf6e6a43089bf56e/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + ':////www.topcreativeformat.com/d5608e4549f32a0daf6e6a43089bf56e/invoke.js"></scr' + 'ipt>');
